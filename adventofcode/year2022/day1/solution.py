7916c6f862a067d6486a6738c7e1b37324cb9919
Thijs <13253091+Thijss@users.noreply.github.com>
2023-12-01 23:26:07 +0100

    Check for missing typehints

---
+++ adventofcode/year2022/day1/solution.py
@@ -0,0 +1,10 @@
+from adventofcode.lib import load_data
+
+
+def part1(file_name: str) -> int:
+    data = load_data(year=2023, day=1, file_name=file_name)
+    return 0
+
+def part2(file_name: str) -> int:
+    data = load_data(year=2023, day=1, file_name=file_name)
+    return 0
