561471e8e6d04ad3e5e925f6b2a5ef416296d706
Thijs Baaijen <Thijss@users.noreply.github.com>
2023-12-01 23:12:38 +0100

    move code to allow for multiple years (#7)
    Co-authored-by: Thijs <13253091+Thijss@users.noreply.github.com>

--- add_day.sh
+++ add_day.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
 # Check if an argument is provided
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 <integer>"
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <year> <day>"
     exit 1
 fi
 
-day=$1
+year=$1
+day=$2
 
 if ! [[ $day =~ ^[0-9]+$ ]]; then
     echo "Error: The provided day should be an integer."
     exit 1
 fi
 
-mkdir adventofcode/day$day
-mkdir adventofcode/day$day/input
-touch adventofcode/day$day/__init__.py
-touch adventofcode/day$day/input/real_data.txt
-touch adventofcode/day$day/input/test_data_part1.txt
-touch adventofcode/day$day/input/test_data_part2.txt
-touch adventofcode/day$day/solution.py
-touch adventofcode/day$day/test_day$day.py
+mkdir adventofcode/year$year/day$day
+mkdir adventofcode/year$year/day$day/input
+touch adventofcode/year$year/day$day/__init__.py
+touch adventofcode/year$year/day$day/input/real_data.txt
+touch adventofcode/year$year/day$day/input/test_data_part1.txt
+touch adventofcode/year$year/day$day/input/test_data_part2.txt
+touch adventofcode/year$year/day$day/solution.py
+touch adventofcode/year$year/day$day/test_day$day.py
