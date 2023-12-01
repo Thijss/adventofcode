99ba867eca90efb89ab03943500da28670689b6d
Thijs <13253091+Thijss@users.noreply.github.com>
2023-12-01 23:28:20 +0100

    Check for missing typehints

--- adventofcode/lib.py
+++ adventofcode/lib.py
@@ -1,10 +1,9 @@
 from pathlib import Path
 
 _ROOT_DIR = Path(__file__).parent
-print(_ROOT_DIR)
 
 
-def load_data(year: int, day: int, file_name: str):
+def load_data(year: int, day: int, file_name: str) -> str:
     path = _ROOT_DIR / f"year{year}" / f"day{day}" / "input" / file_name
     with open(path) as f:
         return f.read()
