c9235b68fe408329f7bf947e48d6372b6c74c055
Thijs <13253091+Thijss@users.noreply.github.com>
2023-12-02 19:29:03 +0100

    Add day4 2022

--- adventofcode/year2022/day4/test_day4.py
+++ adventofcode/year2022/day4/test_day4.py
@@ -1,21 +1,21 @@
-from adventofcode.year2022.day2.solution import part1, part2
+from adventofcode.year2022.day4.solution import part1, part2
 
 
 def test_part1_test_data() -> None:
     answer = part1("test_data.txt")
-    assert 15 == answer
+    assert 2 == answer
 
 
 def test_part1_real_data() -> None:
     answer = part1("real_data.txt")
-    assert 14163 == answer
+    assert 456 == answer
 
 
 def test_part2_test_data() -> None:
     answer = part2("test_data.txt")
-    assert 12 == answer
+    assert 4 == answer
 
 
 def test_part2_real_data() -> None:
     answer = part2("real_data.txt")
-    assert 12091 == answer
+    assert 808 == answer
