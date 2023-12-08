1052e0ee13092cf3f7d619b6d195d3a35b2e1266
Thijs Baaijen <thijs@Thijs-Baaijens-MacBook-Pro.local>
2023-12-08 09:39:04 +0100

    Fix test names

--- adventofcode/year2023/day3/test_day3.py
+++ adventofcode/year2023/day3/test_day3.py
@@ -1,21 +1,21 @@
 from adventofcode.year2023.day3.solution import part1, part2
 
 
-def test_day1_part1_test_data() -> None:
+def test_part1_test_data() -> None:
     answer = part1("test_data.txt")
     assert 4361 == answer
 
 
-def test_day1_part1_real_data() -> None:
+def test_part1_real_data() -> None:
     answer = part1("real_data.txt")
     assert 539590 == answer
 
 
-def test_day1_part2_test_data() -> None:
+def test_part2_test_data() -> None:
     answer = part2("test_data.txt")
     assert 467835 == answer
 
 
-def test_day1_part2_real_data() -> None:
+def test_part2_real_data() -> None:
     answer = part2("real_data.txt")
     assert 80703636 == answer
--- adventofcode/year2023/day6/test_day6.py
+++ adventofcode/year2023/day6/test_day6.py
@@ -1,21 +1,21 @@
 from adventofcode.year2023.day6.solution import part1, part2
 
 
-def test_day1_part1_test_data() -> None:
+def test_part1_test_data() -> None:
     answer = part1("test_data.txt")
     assert 288 == answer
 
 
-def test_day1_part1_real_data() -> None:
+def test_part1_real_data() -> None:
     answer = part1("real_data.txt")
     assert 252000 == answer
 
 
-def test_day1_part2_test_data() -> None:
+def test_part2_test_data() -> None:
     answer = part2("test_data.txt")
     assert 71503 == answer
 
 
-def test_day1_part2_real_data() -> None:
+def test_part2_real_data() -> None:
     answer = part2("real_data.txt")
     assert 36992486 == answer
