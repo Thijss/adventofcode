90c953463af69c76464c40c4f8bf682e5f25e418
Thijs Baaijen <Thijss@users.noreply.github.com>
2023-12-08 09:46:38 +0100

    Fix test names (#25)
    * Fix test names
    
    * cleanup
    
    * poetry update
    
    ---------
    
    Co-authored-by: Thijs Baaijen <thijs@Thijs-Baaijens-MacBook-Pro.local>
    Co-authored-by: Thijss <13253091+Thijss@users.noreply.github.com>

--- adventofcode/year2023/day1/test_day1.py
+++ adventofcode/year2023/day1/test_day1.py
@@ -1,21 +1,21 @@
 from adventofcode.year2023.day1.solution import part1, part2
 
 
-def test_day1_part1_test_data() -> None:
+def test_part1_test_data() -> None:
     answer = part1("test_data_part1.txt")
     assert 142 == answer
 
 
-def test_day1_part1_real_data() -> None:
+def test_part1_real_data() -> None:
     answer = part1("real_data.txt")
     assert 53194 == answer
 
 
-def test_day1_part2_test_data() -> None:
+def test_part2_test_data() -> None:
     answer = part2("test_data_part2.txt")
     assert 281 == answer
 
 
-def test_day1_part2_real_data() -> None:
+def test_part2_real_data() -> None:
     answer = part2("real_data.txt")
     assert 54249 == answer
