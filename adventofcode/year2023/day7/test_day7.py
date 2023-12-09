f44ca5e6c393477eb5eab3528f087f9cf518d00c
Thijss <13253091+Thijss@users.noreply.github.com>
2023-12-09 12:34:47 +0100

    Add 2023 day 7

--- adventofcode/year2023/day7/test_day7.py
+++ adventofcode/year2023/day7/test_day7.py
@@ -3,19 +3,19 @@ from adventofcode.year2023.day7.solution import part1, part2
 
 def test_part1_test_data() -> None:
     answer = part1("test_data.txt")
-    assert 288 == answer
+    assert 6440 == answer
 
 
 def test_part1_real_data() -> None:
     answer = part1("real_data.txt")
-    assert 252000 == answer
+    assert 249483956 == answer
 
 
 def test_part2_test_data() -> None:
     answer = part2("test_data.txt")
-    assert 71503 == answer
+    assert 5905 == answer
 
 
 def test_part2_real_data() -> None:
     answer = part2("real_data.txt")
-    assert 36992486 == answer
+    assert 252137472 == answer
