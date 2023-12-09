920e20698e080be147b8bb22ddcf5b0d265d1685
Thijss <13253091+Thijss@users.noreply.github.com>
2023-12-09 13:01:12 +0100

    update testdata

--- adventofcode/year2023/day8/test_day8.py
+++ adventofcode/year2023/day8/test_day8.py
@@ -2,7 +2,7 @@ from adventofcode.year2023.day8.solution import part1, part2
 
 
 def test_part1_test_data() -> None:
-    answer = part1("test_data.txt")
+    answer = part1("test_data_part1.txt")
     assert 6 == answer
 
 
@@ -12,7 +12,7 @@ def test_part1_real_data() -> None:
 
 
 def test_part2_test_data() -> None:
-    answer = part2("test_data.txt")
+    answer = part2("test_data_part2.txt")
     assert -1 == answer
 
 
